301f13bc6c08654f8e13b48f9e289b5d7a060e33
Onix <122640335+On1x-dot@users.noreply.github.com>
2024-10-06 23:08:03 -0600

    Add-Comet
    Add a comet style

--- NASA-App/src/App.tsx
+++ NASA-App/src/App.tsx
@@ -1,21 +1,33 @@
-// src/App.tsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Home from "./views/Home";
 import Intro from "./views/Intro"; // Asegúrate de que la ruta sea correcta
+import Comet from "./components/Planets/Comet"; // Importa el componente Comet
 import './App.css'; // Asegúrate de que los estilos estén importados
 
 const App: React.FC = () => {
   const [showHome, setShowHome] = useState(false);
+  const [showComet, setShowComet] = useState(false);
 
   const handleStart = () => {
     setShowHome(true); // Cambia a la vista de Home al hacer clic en "Start"
   };
+
+  useEffect(() => {
+    // Temporizador para mostrar el cometa después de 10 segundos
+    const timer = setTimeout(() => {
+      setShowComet(true); // Muestra el cometa
+    }, 10000); // 10 segundos en milisegundos
+
+    // Limpieza del temporizador
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div>
       {showHome ? (
         <div className="fade-in">
           <Home />
+          {showComet && <Comet />} {/* Muestra el cometa solo si showComet es true */}
         </div>
       ) : (
         <Intro onStart={handleStart} />
---
+++ NASA-App/src/components/Planets/Comet.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../styles/Planets/Comet.css';
+
+const Comet: React.FC = () => {
+  return (
+    <div className="comet"></div>
+  );
+};
+
+export default Comet;
